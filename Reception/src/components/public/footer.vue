8a063046633eabb57b52ba5102f276d6cdd98662
suyuchen <suyuchencom@163.com>
2019-04-28 13:44:10 +0800

    h5--搭建底部菜单栏

--- Reception/src/components/public/footer.vue
+++ Reception/src/components/public/footer.vue
@@ -1,6 +1,29 @@
 <template>
     <div id="Footer">
-        123
+        <div>
+            <p class="el-icon-s-shop"></p>
+            <p>
+                商店
+            </p>
+        </div>
+        <div>
+            <p class="el-icon-menu"></p>
+            <p>
+                分类
+            </p>
+        </div>
+        <div>
+            <p class="el-icon-shopping-cart-full"></p>
+            <p>
+                购物车
+            </p>
+        </div>
+        <div>
+            <p class="el-icon-user"></p>
+            <p>
+                个人
+            </p>
+        </div>
     </div>
 </template>
 
@@ -13,7 +36,27 @@
 <style scoped lang="scss">
     #Footer{
         width: 100%;
-        height: .4rem;
-        background-color: #00a2ca;
+        height: .46rem;
+        background-color: #ffffff;
+        display: flex;
+        justify-content: space-between;
+        position: fixed;
+        bottom: 0px;
+        box-shadow: .01rem .01rem .01rem .01rem;
+        /*background-color: black;*/
+        div{
+            p{
+                color: black;
+            }
+            p:nth-child(1){
+                margin-top: .05rem;
+            }
+        }
+        div:nth-child(1){
+            margin-left: .15rem;
+        }
+        div:nth-child(4){
+            margin-right: .15rem;
+        }
     }
 </style>
--- Reception/src/main.js
+++ Reception/src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import '@/assets/static/css/global.css'
+import '@/assets/css/global.css'
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
